95de24966b132dbda3fc45abfe1b690cda645f97
M-Mafeni <mm17291@bristol.ac.uk>
2020-03-04 18:34:33 +0000

    changed javascript style
    Co-Authored-By: M-Mafeni <35997242+M-Mafeni@users.noreply.github.com>
    Co-Authored-By: ainesh1998 <ainesh1998@outlook.com>

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop()>300){
-            $('nav').addClass('bar_colour');
-        }else{
-            $('nav').removeClass('bar_colour');
-        }
-    });
-
-});
+$(document).ready(start);
+function start(){
+    $(window).scroll(change_bar);
+}
+function change_bar() {
+    if($(window).scrollTop()>300){
+        $('nav').addClass('bar_colour');
+    }else{
+        $('nav').removeClass('bar_colour');
+    }
+}
