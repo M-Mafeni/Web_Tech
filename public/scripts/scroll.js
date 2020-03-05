e86e2527ecef554fc6fd300c231828fbb9ea8fb4
ainesh1998 <ainesh1998@outlook.com>
2020-03-05 17:24:18 +0000

    removed watermark and fixed the "about us" jump when the link is clicked

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -3,7 +3,7 @@ function start(){
     $(window).scroll(change_bar);
 }
 function change_bar() {
-    if($(window).scrollTop()>700){
+    if($(window).scrollTop()>630){
         $('nav').addClass('bar_colour');
     }else{
         $('nav').removeClass('bar_colour');
