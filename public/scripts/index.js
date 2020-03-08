0335bdd6323b4887e0b7f933e088a861bb97c7a9
M-Mafeni <mm17291@bristol.ac.uk>
2020-03-08 00:47:12 +0000

    tried making it responsive

---
+++ public/scripts/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+$(document).ready(start);
+function start(){
+    $(window).scroll(change_bar);
+
+    var modal = document.getElementById("loginForm");
+    window.onclick = function(event) {
+      // console.log("clicked");
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+function change_bar() {
+    if($(window).scrollTop()>630){
+        $('nav').addClass('bar_colour');
+    }else{
+        $('nav').removeClass('bar_colour');
+    }
+}
+
+function openForm() {
+  document.getElementById("loginForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("loginForm").style.display = "none";
+}
+
+function makeBarResponsive() {
+  console.log($('nav').hasClass('responsive'));
+  if(!($('nav').hasClass('responsive'))){
+      $('nav').addClass('responsive');
+  }else{
+      $('nav').removeClass('responsive');
+  }
+}
