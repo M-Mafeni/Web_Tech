5fff039ab36bbee04ada120b44eabc8b1822a82e
ainesh1998 <ainesh1998@outlook.com>
2020-03-11 23:11:53 +0000

    details from selected ticket are now displayed in the summary

--- public/scripts/bookings.js
+++ public/scripts/bookings.js
@@ -8,9 +8,22 @@ function start(){
 function ticket_selected() {
     $(".ticket-selected").removeClass("ticket-selected");
     $(this).addClass("ticket-selected");
-    
-    var source = $(this).find(".source").text();
-    var destination = $(this).find(".destination").text();
 
-    console.log(source);
+    var source = $(this).find(".source");
+    var departureTime = source.find(".time").text();
+    var departureLoc = source.find(".location").text();
+
+    var destination = $(this).find(".destination");
+    var arrivalTime = destination.find(".time").text();
+    var arrivalLoc = destination.find(".location").text();
+
+    var price = $(this).find(".price").text();
+
+    // console.log(departureTime);
+    // console.log(departureLoc);
+    // console.log(arrivalTime);
+    // console.log(arrivalLoc);
+    // console.log(price);
+
+    $("#summary_details").html(departureTime + " " + departureLoc + "<br/>" + arrivalTime + " " + arrivalLoc + "<br/>" + price);
 }
