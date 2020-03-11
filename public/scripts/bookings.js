25f87fc220a11e93d6df3b87f2acda2c4d14945e
ainesh1998 <ainesh1998@outlook.com>
2020-03-11 22:37:16 +0000

    button selection works, now data must be extracted

--- public/scripts/bookings.js
+++ public/scripts/bookings.js
@@ -2,7 +2,15 @@
 
 $(document).ready(start);
 function start(){
-    $('#ticket_card').click(function() {
-        console.log('clicked');
-    });
+    $(".ticket-card").click(ticket_selected);
+}
+
+function ticket_selected() {
+    $(".ticket-selected").removeClass("ticket-selected");
+    $(this).addClass("ticket-selected");
+    
+    var source = $(this).find(".source").text();
+    var destination = $(this).find(".destination").text();
+
+    console.log(source);
 }
