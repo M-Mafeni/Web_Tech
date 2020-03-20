22bf8d3e3c627014d3ac75ad21a8b440b238da23
M-Mafeni <mm17291@bristol.ac.uk>
2020-03-20 17:06:31 +0000

    Using Express+Handlebars. NOw gets tickets from database

--- express.js
+++ express.js
@@ -1,15 +1,35 @@
 "use strict";
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const api = require('./api');
 const app = express();
 const port = 8080;
+let OK = 200, NotFound = 404, BadType = 415, Error = 500;
 
-start();
+app.set('views', './views');
+app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 
-function start() {
-    app.use(express.static('public'));
-    app.get('/bookings.html',function (req,res) {
-        console.log("received");
-        // res.sendFile('bookings.html');
+app.engine('handlebars', exphbs({
+    layoutsDir: __dirname + '/views/layouts'
+}));
+app.use(express.static('public'));
+//app.use(express.static('views'));
+
+//app.use(api);
+app.get('/',function (req,res) {
+    res.render('main',{layout:'index'});
+});
+
+app.get('/bookings.html',function (req,res) {
+    let sql = "SELECT * FROM Ticket";
+    api.db.all(sql,[],(err,tickets) => {
+        if(err){
+            throw err;
+        }
+        res.render('main',{layout:'bookings',
+                   tickets: tickets  });
     });
-    app.listen(port, () => console.log(`listening on port ${port}!`));
-}
+;
+});
+app.listen(port, () => console.log(`listening on port ${port}!`));
