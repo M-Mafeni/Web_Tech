547e0e5297de9a8b320a7729d77aed05239f4d08
M-Mafeni <mm17291@bristol.ac.uk>
2020-03-21 18:24:44 +0000

    made basic skeleton for login form

--- express.js
+++ express.js
@@ -3,22 +3,23 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const api = require('./api');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
 const sqlite = require('sqlite3').verbose();
 let db = new sqlite.Database('./astra.db');
 let OK = 200, NotFound = 404, BadType = 415, Error = 500;
 
 app.set('views', './views');
-app.set('view engine', 'hbs');
 app.set('view engine', 'handlebars');
 
 app.engine('handlebars', exphbs({
     layoutsDir: __dirname + '/views/layouts'
 }));
 app.use(express.static('public'));
-//app.use(express.static('views'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(api);
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
 app.get('/',function (req,res) {
     res.render('main',{layout:'index'});
 });
@@ -37,4 +38,21 @@ app.get('/bookings.html',function (req,res) {
                    tickets: tickets  });
     });
 });
-app.listen(port, () => console.log(`listening on port ${port}!`));
+
+app.post('/login',function(req,res){
+    let sql = "SELECT * FROM User WHERE email = ? AND password = ?";
+    let email =req.body.email;
+    let password = req.body.psw;
+    db.get(sql,[email,password],(err,user) => {
+    if(user != undefined){
+        res.redirect('/');
+    }else{
+        res.redirect('/login?error');
+    }
+    });
+});
+
+app.get('/login',function(req,res){
+    res.redirect('/');
+})
+
