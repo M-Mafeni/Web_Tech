eacad03da7f44a1c47f6404912eeb0c384253aba
M-Mafeni <mm17291@bristol.ac.uk>
2020-03-22 15:04:20 +0000

    added comments and fixed minor issues

--- express.js
+++ express.js
@@ -9,17 +9,23 @@ const sqlite = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 const df = require('./format')
 
+//connect to astra.db database
 let db = new sqlite.Database('./astra.db');
 let OK = 200, NotFound = 404, BadType = 415, Error = 500;
 
+//set views(html layouts) to be your local views directory
 app.set('views', './views');
+//set handlebars as templating engine
 app.set('view engine', 'handlebars');
-
+//set the layouts directory for the handlebars engine
 app.engine('handlebars', exphbs({
     layoutsDir: __dirname + '/views/layouts'
 }));
+//this gives express access to all files in public
 app.use(express.static('public'));
+//this allows you to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+//session is needed to save that a user is logged in
 app.use(session({
 	secret: 'secret',
 	resave: true,
@@ -28,12 +34,17 @@ app.use(session({
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
 
+//for the main page return main.handlebars with its layout being the index page
 app.get('/',function (req,res) {
     res.render('main',{layout:'index'});
 });
 
+
 app.get('/bookings.html',function (req,res) {
-    if(req.session.loggedIn|| !isEmpty(req.query)){
+    /*only go to the bookings page if the user is logged in and they have
+     entered a query i.e localhost:8080/bookings.html wouldn't work on its own
+    */
+    if(req.session.loggedin && !isEmpty(req.query)){
         let origin = req.query.origin;
         let destination = req.query.destination;
         let date = req.query.date;
@@ -48,7 +59,7 @@ app.get('/bookings.html',function (req,res) {
                 ticket.o_date = df.formatDate(ticket.o_date);
                 ticket.d_date = df.formatDate(ticket.d_date);
             });
-
+            //add tickets attribute to main (used for templating in bookings page)
             res.render('main',{layout:'bookings',
                        tickets: tickets  });
         });
@@ -66,10 +77,15 @@ app.post('/login',function(req,res){
     let password = req.body.psw;
     db.get(sql,[email],(err,user) => {
         if(user == undefined){
+            /*for now its sending you to an error page.
+            should change this to be a prompt instead*/
             res.send("Incorrect Email");
         }else{
+            //password is hashed using bcrypt so hashes need to be compared
+            //user.password = hash from db
             bcrypt.compare(password,user.password,function(err,valid){
                 if(valid){
+                    //user is logged in with that username
                     req.session.loggedin = true;
                     req.session.username = email;
                     res.redirect('/');
