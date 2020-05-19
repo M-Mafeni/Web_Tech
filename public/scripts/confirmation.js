71e9c2793231ff59860980d129583927450d489b
ainesh1998 <ainesh1998@outlook.com>
2020-05-19 10:49:59 +0800

    added fake card

--- public/scripts/confirmation.js
+++ public/scripts/confirmation.js
@@ -2,7 +2,17 @@
 
 $(document).ready(start);
 
-function start(){
+function start() {
     $('input[name="out_id"]').val($(this).find(".ticket_id").text()[0]);
     $('input[name="in_id"]').val($(this).find(".ticket_id").text()[1]);
+    $("#payment").change(showPayButton)
+}
+
+function showPayButton() {
+    if ($("#payment").val() != "Select card") {
+        $("#payButton").show();
+    }
+    else {
+        $("#payButton").hide();
+    }
 }
