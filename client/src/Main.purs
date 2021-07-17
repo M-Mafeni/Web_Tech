61a3d68f66c23b315029a631e3030953c2a297aa
M-Mafeni <mafeni99@gmail.com>
2021-07-17 02:36:41 +0100

    Add Footer Component

--- client/src/Main.purs
+++ client/src/Main.purs
@@ -1,9 +1,25 @@
 module Main where
 
 import Prelude
+
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Exception (throw)
+import React.Basic.DOM as ReactDom
+import Web.DOM.Document (toNonElementParentNode)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toDocument)
+import Web.HTML.Window (document)
+import Components.Footer (mkFooterComponent)
+
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  doc <- document =<< window
+  val <- getElementById "app" $ toNonElementParentNode $ toDocument doc
+  case val of
+    Nothing -> throw "Could not find element with id = app."
+    Just app -> do
+      footer <- mkFooterComponent 
+      ReactDom.render (footer unit) app
