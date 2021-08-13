41bf7db6fbe9ac3ff0507fa0fab9e32dcdc3787b
M-Mafeni <mafeni99@gmail.com>
2021-08-14 00:58:55 +0100

    Display tickets in summary block when clicked

--- client/src/Components/Bookings/BookingsPage.purs
+++ client/src/Components/Bookings/BookingsPage.purs
@@ -1,23 +1,25 @@
 module Components.BookingsPage (mkBookingsPageComponent) where
 
 import Prelude
-
 import Components.NavBar (mkNavBarComponent)
 import Context as Context
 import Data.Array (null)
 import Data.BookingsSearch (BookingsSearch, getBookingTickets)
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Data.Monoid (guard)
 import Data.Ticket (Ticket)
 import Data.Tuple.Nested ((/\))
+import Effect (Effect)
 import Effect.Aff (runAff_, throwError)
 import Effect.Exception (throw)
 import Prim.TypeError (class Warn, Text)
 import React.Basic.DOM as DOM
+import React.Basic.Events as Event
 import React.Basic.Hooks (JSX)
 import React.Basic.Hooks as React
 
-mobileSummaryBlock :: JSX
+mobileSummaryBlock :: (Warn (Text "Add setTicket functionality in Mobile Summary Block")) => JSX
 mobileSummaryBlock =
   DOM.div
     { className: "summary-icon"
@@ -99,48 +101,73 @@ hiddenForm =
       , "d_loc"
       ]
 
-summaryBlock :: JSX
-summaryBlock =
+summaryBlock :: (Maybe Ticket) -> (Maybe Ticket) -> JSX
+summaryBlock finalOutboundTicket finalInboundTicket =
   DOM.div
     { className: "column summary"
     , id: "summary"
     , children:
         [ DOM.h1_ [ DOM.text "Summary" ]
-        , DOM.p
-            { className: "o_unselected"
-            , children: [ DOM.text "No outbound ticket selected" ]
-            }
-        , DOM.div { id: "summary_price_final" }
-        , DOM.div { id: "summary_price", className: "summary_price" }
-        , mkSummaryDetails false
-        , DOM.p
-            { className: "d_unselected"
-            , children: [ DOM.text "No inbound ticket selected" ]
-            }
-        , DOM.div { id: "d_summary_price", className: "summary_price" }
-        , mkSummaryDetails true
+        , DOM.div { id: "summary_price_final", children: [ DOM.text finalPriceText ] }
+        , mkOutboundTicketDetails
+        , mkInboundTicketDetails
         , hiddenForm
         ]
     }
   where
-  mkSummaryDetails isInbound =
-    DOM.div
-      { id: (guard isInbound "d_") <> "summary_details"
-      , className: "summary_details"
-      , children: mkDetails true <> mkDetails false
-      }
+  finalPriceText = case (finalOutboundTicket /\ finalInboundTicket) of
+    (Nothing /\ Nothing) -> ""
+    (Just t1 /\ Just t2) ->  "£" <> (show $ t1.price + t2.price)
+    (Just t1 /\ Nothing) -> "£" <> show t1.price
+    (Nothing /\ Just t2) -> "£" <> show t2.price
 
-  mkDetails isDeparture =
-    let
-      prefix = if isDeparture then "departure" else "arrival"
-    in
-      [ DOM.div { id: guard (not isDeparture) "arrivalDate", className: "date " <> prefix <> "Date" }
-      , DOM.span { className: "time " <> prefix <> "Time" }
-      , DOM.span { className: "location " <> prefix <> "Location" }
+  mkInboundTicketDetails = case finalInboundTicket of
+    Nothing ->
+      DOM.p
+        { className: "d_unselected"
+        , children: [ DOM.text "No inbound ticket selected" ]
+        }
+    Just ticket -> mkSummaryDetails true ticket
+
+  mkOutboundTicketDetails = case finalOutboundTicket of
+    Nothing ->
+      DOM.p
+        { className: "o_unselected"
+        , children: [ DOM.text "No outbound ticket selected" ]
+        }
+    Just ticket -> mkSummaryDetails false ticket
+
+  mkSummaryDetails isInbound ticket =
+    React.fragment
+      [ DOM.div { id: "summary_price", className: "summary_price", children: [ DOM.text $ "£" <> (show ticket.price) ] }
+      , DOM.div
+          { id: (guard isInbound "d_") <> "summary_details"
+          , className: "summary_details change_height"
+          , children: mkDetails true <> mkDetails false
+          }
       ]
+    where
+    mkDetails isDeparture =
+      let
+        prefix = if isDeparture then "departure" else "arrival"
+      in
+        [ DOM.div
+            { id: guard (not isDeparture) "arrivalDate"
+            , className: "date " <> prefix <> "Date"
+            , children: [ DOM.text (if isDeparture then ticket.d_date else ticket.o_date) ]
+            }
+        , DOM.span
+            { className: "time " <> prefix <> "Time"
+            , children: [ DOM.text $ (if isDeparture then ticket.d_time else ticket.o_time) <> " " ]
+            }
+        , DOM.span
+            { className: "location " <> prefix <> "Location"
+            , children: [ DOM.text (if isDeparture then ticket.destination_place else ticket.origin_place) ]
+            }
+        ]
 
-mkTickets :: (Warn (Text "Add onclick handler in mkTickets")) => String -> Array Ticket -> Array JSX
-mkTickets title tickets = [ DOM.h1 { className: "Journey_Text", children: [ DOM.text title ] } ] <> map mkTicket tickets
+mkTickets :: String -> Array Ticket -> (Maybe Ticket -> Effect Unit) -> Array JSX
+mkTickets title tickets setFinalTicket = [ DOM.h1 { className: "Journey_Text", children: [ DOM.text title ] } ] <> map mkTicket tickets
   where
   mkTicket :: Ticket -> JSX
   mkTicket ticket =
@@ -159,6 +186,7 @@ mkTickets title tickets = [ DOM.h1 { className: "Journey_Text", children: [ DOM.
               }
           , mkDate ticket.d_date ticket.d_time ticket.destination_place false
           ]
+      , onClick: Event.handler_ (setFinalTicket (Just ticket))
       }
     where
     mkDate date time place isSource =
@@ -180,7 +208,9 @@ mkBookingsPageComponent = do
     let
       noTickets = DOM.p_ [ DOM.text "No tickets available for this journey" ]
     outboundTickets /\ setOutboundTickets <- React.useState' []
-    inboundTickets /\ setInboundTickets <- React.useState' [] 
+    inboundTickets /\ setInboundTickets <- React.useState' []
+    finalOutboundTicket /\ setFinalOutboundTicket <- React.useState' Nothing
+    finalInboundTicket /\ setFinalInboundTicket <- React.useState' Nothing
     React.useEffectOnce do
       flip runAff_ (getBookingTickets bookingsSearch) \res -> case res of
         Left err -> throwError err
@@ -197,34 +227,33 @@ mkBookingsPageComponent = do
           , children:
               [ DOM.div
                   { id: "outbound"
-                  , children: mkTickets "Outbound Journeys" outboundTickets
+                  , children: mkTickets "Outbound Journeys" outboundTickets setFinalOutboundTicket
                   }
               , if null inboundTickets then
                   noTickets
                 else
                   DOM.div
                     { id: "inbound"
-                    , children: mkTickets "Inbound Journeys" inboundTickets
+                    , children: mkTickets "Inbound Journeys" inboundTickets setFinalInboundTicket
                     }
               ]
           }
     pure
-      $ DOM.div 
-        { className: "bookings-container"
-        , children: 
-          [ navbar { isMainPage: false }
-          , mobileSummaryBlock
-          , DOM.div
-              { className: "row"
-              , children:
-                  if null outboundTickets then
-                    [ noTickets ]
-                  else
-                    [ columnTickets
-                    , DOM.span { id: "summaryTop" }
-                    , summaryBlock
-                    ]
-              }
-          ]
-        }
-
+      $ DOM.div
+          { className: "bookings-container"
+          , children:
+              [ navbar { isMainPage: false }
+              , mobileSummaryBlock
+              , DOM.div
+                  { className: "row"
+                  , children:
+                      if null outboundTickets then
+                        [ noTickets ]
+                      else
+                        [ columnTickets
+                        , DOM.span { id: "summaryTop" }
+                        , summaryBlock finalOutboundTicket finalInboundTicket
+                        ]
+                  }
+              ]
+          }
